3005c05a5c817b8d0521c5ab41cd6e6ee3219f0e
vikushaaa <117160086+vikushaaa@users.noreply.github.com>
2022-11-08 12:38:39 +0200

    зробила завдання

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+var input1 = document.getElementyById("task1"),
+res = document.getElementyById("result"),
+button= document.getElementyById("button"),
+counter = 0;
+button.addEventListener("click",
+function(){
+)
+if(input1.value=="GitHub"){
+counter++
 
+}
+else{
+result="0"
+}
